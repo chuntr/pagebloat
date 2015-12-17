eae228522886fe7b88cf8e748f7893d35dee999e
chuntr <chunter@denormal.org>
2015-12-17 15:49:27 -0800

    fixed sidebar formatting and added subdomains for stackexchange sites

--- bloater.js
+++ bloater.js
@@ -1,26 +1,24 @@
 // ==UserScript==
-// @name         bloater
+// @name         widenBlocks
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  Tampermonkey script that resizes the default column widths for some popular sites to take advantage of wider monitors
+// @description  try to take over the world!
 // @author       Christopher Hunter <chunter@denormal.org>
 // @match        http://*.readthedocs.org/*
 // @match        http://stackexchange.com/*
+// @match        http://*.stackexchange.com/*
 // @match        http://stackoverflow.com/*
 // @match        http://serverfault.com/*
 // @grant        none
 // ==/UserScript==
 /* jshint -W097 */
 'use strict';
-
 function jQsetWidthByClassName(width,className){
     jQuery(className).css('width', width);
 }
-
 function jQsetMaxWidthByClassName(pxWidth,className){
     jQuery(className).css('max-width', pxWidth);
 }
-
 function setWidthByClassName(width,className){
     var elements = document.getElementsByClassName(className);
     for (var i=0; i<elements.length; i++){
@@ -28,7 +26,6 @@ function setWidthByClassName(width,className){
         elements[i].style.width = width;
     }
 }
-
 function setWidthById(width,id){
     var element = document.getElementById(id);
     element.setAttribute("style","width:"+width+";");
@@ -40,8 +37,8 @@ jQsetWidthByClassName(1024,"div.rst-content");
 jQsetMaxWidthByClassName("1128px","div.wy-nav-content");
 
 // stackexchange
-setWidthById("1200px","answers-header");
-setWidthById("1200px","content");
+setWidthById("1400px","content");
 setWidthById("1100px","mainbar");
 setWidthById("1100px","question");
 setWidthByClassName("1000px","post-text");
+setWidthById("1000px","answers");
