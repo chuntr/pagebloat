bde36124f7fa6ca66e83e86ba822a0c84f8be848
Christopher Hunter <chunter@denormal.org>
2016-10-24 11:22:48 -0700

    more updates

--- bloater.js
+++ bloater.js
@@ -1,7 +1,8 @@
 // ==UserScript==
 // @name        bloater
+// @author      Christopher Hunter
 // @namespace   http://github.com/chuntr/
-// @source      https://raw.githubusercontent.com/chuntr/pagebloat/master/bloater.js
+// @source      https://github.com/chuntr/pagebloat
 // @version     0.2.1
 // @updateURL   https://raw.githubusercontent.com/chuntr/pagebloat/master/bloater.js
 // @description Tampermonkey script that resizes the default column widths for some popular sites to take advantage of wider monitors
